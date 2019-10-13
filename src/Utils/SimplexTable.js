61201f363d510d334633678a7450f31b744497bd
Filipe Jesse <filipejesse5@gmail.com>
2019-10-13 16:16:04 -0300

    Add TwoSteps method to create the first step table

--- src/Utils/SimplexTable.js
+++ src/Utils/SimplexTable.js
@@ -4,7 +4,7 @@ class SimplexTable{
   calcPP(column, tableSimplex){
     let pp = [];
     tableSimplex.forEach(x => {
-      let temp = x[length(x)-1]/x[column];
+      let temp = x[x.length-1]/x[column];
       if (temp >= 0)
         pp.push(temp); 
     });
@@ -26,7 +26,7 @@ class SimplexTable{
   resolveSimplex(table){
     let columnFocus;
     let lineFocus = 0;
-    let z = table[length(table) - 1];
+    let z = table[table.length - 1];
     z.pop();
 
     if(Math.min.apply(null, z) >= 0){
@@ -61,5 +61,105 @@ class SimplexTable{
 
     return this.resolveSimplex(table);
   }
+
+
+  TwoSteps(variables, numFolgas, numAritificiais, restricoes, f_objetiva){
+    
+    let table = [];
+    
+    let countInFolgas = 0;
+    let countInArt = 0;
+
+    //insere as restrições na tabela
+    restricoes.forEach((x,i) => {
+        let temp = new Array(variables.length + numFolgas + numAritificiais + 1);
+        
+        temp.forEach((x, i) => {
+            temp[i] = 0;
+        });
+        
+        for(let j = 0; j < variables.length; j++ ){
+            temp[j] = x[j]; 
+        }
+
+        switch(x[variables.length]){
+            case "<=":
+                temp[variables.length + countInFolgas] = 1;
+                countInFolgas++;
+                break;
+        
+            case ">=":
+                temp[variables.length + numFolgas + countInArt] = 1;
+                countInArt++;
+                break;
+            
+            case "=":
+                temp[variables.length + countInFolgas] = -1;
+                temp[variables.length + numFolgas + countInArt] = 1;
+                countInArt++;
+                countInFolgas++;
+                break;
+
+            default:
+                throw "restrição inválida!";
+        }
+
+        temp[temp.length - 1] = x[x.length -1];
+      
+        table.push(temp);
+    });
+
+    //insere a função objetiva (z) na tabela
+    let z = new Array(variables.length + numFolgas + numAritificiais + 1);
+    z.forEach((x, i) => {
+        z[i] = 0;
+    });
+
+    f_objetiva.forEach((x, i) => {
+        if(i < f_objetiva.length - 1)
+            z[i] = x;
+
+        else
+            z[z.length - 1] = x;
+    });
+
+    table.push(z);
+
+
+    //insere z' na tabela
+
+    let zLinha = new Array(variables.length + numFolgas + numAritificiais + 1);
+    zLinha.forEach((x, i) => {
+        zLinha[i] = 0;
+    });
+
+    let linhasArtificiais = [];
+    
+    for(let i = 0; i < table.length - 1; i++){
+        for(let j = 0; j < numAritificiais; j++){
+            if(table[i][variables.length + numFolgas + j] == 1){
+                linhasArtificiais.push(i);
+            }
+        }
+    }
+
+    for(let i = 0; i < variables.length + numFolgas; i++){
+        let sumArt = 0;
+        linhasArtificiais.forEach(x => {
+            sumArt += table[x][i];
+        })
+        zLinha[i] = sumArt;
+    }
+
+    let sumArt = 0;
+    linhasArtificiais.forEach(x => {
+        sumArt += table[x][table[x].length-1];
+    })
+    zLinha[zLinha.length-1] = sumArt;
+
+    table.push(zLinha);
+  }
+
+
 }
 export default SimplexTable;
